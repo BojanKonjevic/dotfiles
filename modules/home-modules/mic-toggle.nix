4e3a49f59e0d554ab6d5a0611533cf93c1e64a3d
BojanKonjevic <konjevicbojan1@gmail.com>
2026-03-27 12:41:16 +0100

    moved mute/unmute into lib

--- modules/home-modules/mic-toggle.nix
+++ modules/home-modules/mic-toggle.nix
@@ -3,9 +3,9 @@
     micToggle = pkgs.writeShellScriptBin "mic-toggle" ''
       wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
       if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q MUTED; then
-        ${pkgs.sox}/bin/sox ${./files/mute.mp3} -t wav - vol 0.25 | pw-play -
+        ${pkgs.sox}/bin/sox ${../lib/mute.mp3} -t wav - vol 0.25 | pw-play -
       else
-        ${pkgs.sox}/bin/sox ${./files/unmute.mp3} -t wav - vol 0.17 | pw-play -
+        ${pkgs.sox}/bin/sox ${../lib/unmute.mp3} -t wav - vol 0.17 | pw-play -
       fi
     '';
   in {
