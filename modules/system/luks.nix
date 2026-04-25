6a0675cbc8e57b822657e977ca1f9e0d9b6115de
BojanKonjevic <konjevicbojan1@gmail.com>
2026-04-25 16:44:10 +0000

    remvoed crypthome from luks

--- modules/system/luks.nix
+++ modules/system/luks.nix
@@ -9,11 +9,5 @@
         device = "/dev/disk/by-partlabel/disk-main-root";
         allowDiscards = true;
       };
-    }
-    // lib.optionalAttrs (userConfig.homeDisk != "") {
-      "crypthome" = {
-        device = "/dev/disk/by-partlabel/disk-main-home";
-        allowDiscards = true;
-      };
     };
 }
