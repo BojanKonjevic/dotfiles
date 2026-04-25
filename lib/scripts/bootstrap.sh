d9b7363711fe03bff71a0ef718950faf4ae4fb79
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-04-25 16:04:20 +0200

    removed misc packages from bootstrap home.nix

--- lib/scripts/bootstrap.sh
+++ lib/scripts/bootstrap.sh
@@ -361,32 +361,6 @@ in {
         nixpkgs.config.allowUnfree = true;
         news.display = "silent";
       }
-
-      # ── Misc packages not yet sorted into a profile ────────────────────────
-      {
-        home.packages = with pkgs; [
-          nvd
-          cachix
-          localsend
-          libreoffice
-          gnome-calculator
-          alsa-utils
-          p7zip
-          unzip
-          dejsonlz4
-          file-roller
-          pavucontrol
-          networkmanagerapplet
-          pinta
-          inputs.agenix.packages.\${pkgs.stdenv.hostPlatform.system}.default
-          glow
-          ripgrep
-          fd
-          duf
-          gdu
-          tree
-        ];
-      }
     ];
   };
 }
