06d9bc1615b49c89090672811878ef7542e000b3
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-07-06 13:12:58 +0200

    fix: syntax

--- lib/scripts/bootstrap-macos.sh
+++ lib/scripts/bootstrap-macos.sh
@@ -19,7 +19,7 @@ echo "════════════════════════�
 if ! command -v nix &>/dev/null; then
   echo "→ Installing Nix..."
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
-    sh -s -- install --no-confirm --volume-encrypt false
+    sh -s -- install macos --no-confirm --volume-encrypt false
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
